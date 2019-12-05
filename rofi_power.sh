f3246dbef3cfcb3cd87b0895a1992ca9be40f4f9
Andreas Schmidt <3750000-djusha@users.noreply.gitlab.com>
2019-12-05 04:44:17 +0000

    Update rofi_power.sh

--- rofi_power.sh
+++ rofi_power.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # ~/scripts/rofi_power.sh
-#
 rofi_cmd="rofi"
 
 # Options
 power_off=" Ausschalten"
 reboot=" Neustarten"
+log_out=" Ausloggen"
 lock=" Sperren"
 suspend=" Ruhemodus"
-log_out=" Ausloggen"
 
-options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
+options="$power_off\n$reboot\n$log_out\n$lock\n$suspend"
 
 chosen=$(echo -e $options | $rofi_cmd -dmenu -selected-row 0)
 
@@ -20,10 +19,10 @@ case $chosen in
     systemctl poweroff ;;
   $reboot)
     systemctl reboot ;;
+  $log_out)
+    i3-msg exit ;;
   $lock)
     light-locker-command -l ;;
   $suspend)
     systemctl syspend ;;
-  $log_out)
-    i3-msg exit ;;
 esac
